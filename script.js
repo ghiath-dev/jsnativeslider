2f888d18df8a6e1290a283a6d254a891c460c41f
holol-dev4 <85062033+holol-dev4@users.noreply.github.com>
2021-06-01 15:01:30 +0300

    adding progress bar

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ class Slider {
         this.prevButton = this.slider.querySelector('.button-left');
         this.nextButton = this.slider.querySelector('.button-right');
         this.interval = this.slider.getAttribute('data-interval') || 3000;
+        this.progressbar = this.slider.querySelector('.progressBar');
+        console.log(this.sliderContainer);
 
         /* Attributes */
         this.currentSlide = 0;
@@ -13,6 +15,8 @@ class Slider {
         this.dragging = false;
         this.timer = null;
         this.transformValue = 0;
+        this.progressBarTimer = null;
+        this.progressBarWidth= 0;
 
         /* Options */
         this.effectiveTransform =  400;
@@ -24,6 +28,8 @@ class Slider {
         this.slider.onmousedown = this.startDrag.bind(this);
         this.slider.onmousemove = this.whileDragging.bind(this);
         this.slider.onmouseup = this.endDrag.bind(this);
+        this.progressBarMove.bind(this);
+
 
         if (this.prevButton != null)
             this.prevButton.onclick = this.previousSlide.bind(this);
@@ -34,18 +40,34 @@ class Slider {
         this.play();
     }
 
-    nextSlide() {
+
+    progressBarMove(){
+        if(this.progressBarWidth >= 100) {
+            this.progressBarWidth = 0;
+            this.render();
+            return;
+        }
+
+        this.progressBarWidth = this.progressBarWidth + 0.1 ;
+        this.render();
+    }
+
+    clearIntervals(){
         clearInterval(this.timer);
+        clearInterval(this.progressBarTimer);
+        this.progressBarWidth = 0;
+    }
+    nextSlide() {
+        this.clearIntervals();
 
         if (this.isLastSlide()) {
             return this.goToFirstSlide();
         }
         this.goToNextSlide();
-        this.play();
     }
 
     previousSlide() {
-        clearInterval(this.timer);
+        this.clearIntervals();
         if (this.isFirstSlide()) {
             return this.goToLastSlide();
         }
@@ -64,30 +86,43 @@ class Slider {
     goToFirstSlide() {
         this.currentSlide = 0;
         this.currentPosition = 0;
+        this.play();
+
         return this.render();
     }
 
     goToNextSlide() {
         this.currentSlide++;
+        this.play();
+
         return this.render();
     }
 
     goToLastSlide() {
         this.currentSlide = this.totalSlides - 1;
+        this.play();
+
         return this.render();
     }
 
 
     goToPreviousSlide() {
         this.currentSlide--;
+        this.play();
+
         return this.render();
     }
 
     play() {
-        if(this.timer != null) clearInterval(this.timer);
+        if(this.timer != null) this.clearIntervals();
         this.timer = setInterval(function (_) {
             this.nextSlide();
         }.bind(this), this.interval);
+        if(this.progressBarTimer)
+            clearInterval(this.progressBarTimer);
+        this.progressBarTimer = setInterval(function () {
+            this.progressBarMove();
+        }.bind(this), (this.interval/1000) );
     }
 
     whileDragging(event) {
@@ -108,10 +143,11 @@ class Slider {
         this.dragging = true;
         this.initialMousePosition = event.clientX;
         this.initialSlidePosition = this.currentPosition;
-        clearInterval(this.timer);
+        this.clearIntervals();
     }
 
-    endDrag() {
+    endDrag(){
+
         this.dragging = false;
         let shouldMove = Math.abs(this.currentPosition - this.initialSlidePosition) >= this.effectiveTransform;
 
@@ -121,15 +157,18 @@ class Slider {
 
         if(shouldMove && this.initialSlidePosition > this.currentPosition && !this.isLastSlide()) {
             this.nextSlide();
+
         }
+        this.play();
+
         this.render();
     }
 
     render() {
         if (!this.dragging)
             this.currentPosition = (-this.currentSlide * window.innerWidth);
-
         this.sliderContainer.style.left = `${this.currentPosition}px`;
+        this.progressbar.style.width = `${this.progressBarWidth}%`
     }
 
 }
